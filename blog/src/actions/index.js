fab4f8af24d8f6cef4d6b50c7a6805b968528c1f
Jeremy DeCarvalho <jeremypdecarvalho@gmail.com>
2019-06-09 14:06:57 -0700

    182 make api calls to users and posts, display info on main app page

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -4,6 +4,20 @@ export const fetchPosts = () => {
 	return async function(dispatch, getState) {
 		//getState not used in this case
 		const response = await jsonPlaceholder.get('/posts');
-		dispatch({type: 'FETCH_POSTS', payload: response })
+		dispatch({type: 'FETCH_POSTS', payload: response.data })
 	}
 }
+
+export const fetchUser = (id) => async dispatch => {
+	const response = await jsonPlaceholder.get(`/users/${id}`);
+
+	dispatch({type: 'FETCH_USER', payload: response.data})
+}
+
+
+// export const fetchUser = (id) => {
+// 	return async function(dispatch, getState){
+// 		const response = await jsonPlaceholder.get(`/user/${id}`)
+// 	}
+// 		dispatch({type: 'FETCH_USER', payload: response.data })
+// }
---
+++ blog/src/components/UserHeader.js
@@ -0,0 +1,36 @@
+import React, {Component} from 'react';
+import { connect } from 'react-redux';
+import { fetchUser } from '../actions';
+
+class UserHeader extends Component {
+	componentDidMount(){
+		// console.log(this.props)
+		this.props.fetchUser(this.props.userId)
+		// console.log('again')
+		// console.log(this.props)
+		//this.props.fetchUser(this.props.userId);
+	}
+
+	render(){
+
+		const { user } = this.props;
+		//console.log(this.props)
+		//const user = this.props.users.find(user => user.id === this.props.userId)
+
+		if(!user){
+			return null;
+		}
+
+		return(
+			<div className="header">{user.name}</div>
+		)
+	}
+}
+
+const mapStateToProps = (state, ownProps) => {
+	//console.log(state)
+	return {user: state.users.find(user => user.id === ownProps.userId) }
+}
+
+export default connect( mapStateToProps, { fetchUser })(UserHeader);
+
